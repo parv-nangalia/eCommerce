d16f6bd3cc3feaa52502c203300e7a9bb6479579
parv-nangalia <nangaliaparv@gmail.com>
2025-08-13 10:25:18 +0530

    chore: Project Setup Residual files
    - Added dependency for webflux in pom
    - Added application.properties
    - Inserting the webclient bean in the main class

--- product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -2,12 +2,21 @@ package com.example.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@Bean
+	public WebClient webClient(WebClient.Builder builder) {
+		// Build a WebClient instance with the base URL for FakeStoreAPI.
+		// This allows all subsequent requests made with this client to be relative to this base URL.
+		return builder.baseUrl("https://fakestoreapi.com").build();
+	}
 }
